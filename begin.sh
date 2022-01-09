94946ae8a47d18149ba7f4cc112ddff4283f90b5
creativeHHD <92228259+creativeHHD@users.noreply.github.com>
2022-01-09 17:44:56 +0700

    Add files via upload

--- begin.sh
+++ begin.sh
@@ -1,8 +1,17 @@
 termux-setup-storage
 
+
 pkg install proot-distro -y
 proot-distro install ubuntu
 pkg install nano
 cd /data/data/com.termux/files/usr/etc
 nano profile
 
+
+proot-distro login ubuntu
+apt-get update
+apt-get install git -y
+git clone https://github.com/creativeHHD/actives
+cd actives
+ls
+sh setup.sh
