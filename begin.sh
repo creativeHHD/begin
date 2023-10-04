6512c42d8173b56c55813825235cd22da5a7703c
creativeHHD <92228259+creativeHHD@users.noreply.github.com>
2023-10-04 15:00:56 +0700

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -1,5 +1,6 @@
 termux-setup-storage
 
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
 
 pkg install proot-distro -y
 proot-distro install ubuntu
@@ -8,4 +9,4 @@ cd /data/data/com.termux/files/usr/etc
 nano profile
 
 
-proot-distro login ubuntu && mv /data/data/com.termux/files/home getgo.sh && sh getgo.sh
+proot-distro login ubuntu && mv /data/data/com.termux/files/home ./start-ubuntu.sh
