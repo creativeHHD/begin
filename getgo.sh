544ec8bc77d07d3cb5096d3eec3d508979b1eda4
creativeHHD <92228259+creativeHHD@users.noreply.github.com>
2023-10-06 15:30:49 +0700

    Update getgo.sh

--- getgo.sh
+++ getgo.sh
@@ -1,6 +1,14 @@
 apt-get update
-apt-get install git -y
-git clone https://github.com/creativeHHD/actives
-cd actives
-sh setup.sh
+apt-get upgrade
+apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
+git clone https://github.com/creativeHHD/miner.git
+apt install python3
+mv ./miner/startactive.py ./ccminer
+cd ccminer
+chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh
+./build.sh
+nano run
 
+cd && cd ../etc
+nano bash.bashrc
