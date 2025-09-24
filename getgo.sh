6a78c2807505552bf5e464512f9fe1bb1b9124db
creativeHHD <92228259+creativeHHD@users.noreply.github.com>
2025-09-24 19:56:37 +0700

    Update getgo.sh

--- getgo.sh
+++ getgo.sh
@@ -6,10 +6,13 @@ git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
 git clone https://github.com/creativeHHD/miner.git
 apt install python3 -y
 mv ./miner/startactive.py ./ccminer && mv ./miner/finish.py ./ccminer
-
 cd ccminer
 chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh
 ./build.sh
+nano run
+
+cd && cd ../etc
+nano bash.bashrc
 
 cd 
 cd ccminer 
