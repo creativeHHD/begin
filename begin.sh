efdd92d5754e073ab023450ef5ffdf6486db1b2f
creativeHHD <92228259+creativeHHD@users.noreply.github.com>
2025-10-02 00:30:57 +0700

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -1,8 +1,8 @@
 termux-setup-storage
 
-pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
+pkg install wget openssl-tool proot && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
 
-pkg install proot-distro -y
+pkg install proot-distro
 proot-distro install ubuntu
 pkg install nano
 
