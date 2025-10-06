9cfda3651ad8574b2773cc325026a279f5b4f060
creativeHHD <92228259+creativeHHD@users.noreply.github.com>
2025-10-06 23:11:00 +0700

    Update getgo.sh

--- getgo.sh
+++ getgo.sh
@@ -4,8 +4,7 @@ apt-get upgrade
 apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
 git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
 git clone https://github.com/creativeHHD/miner.git
-apt install python3 -y
-pip install requests
+
 mv ./miner/startactive.py ./ccminer && mv ./miner/finish.py ./ccminer
 mv ./miner/set_token.py ./ccminer && mv ./miner/active_run.py ./ccminer
 cd ccminer
